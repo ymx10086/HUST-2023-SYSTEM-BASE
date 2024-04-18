6c03c10c2550902233563241fb4d13e2a5717fd6
杨明欣 <1411477833@qq.com>
2024-04-18 09:36:00 +0800

    PROGRAM

---
+++ CS2106_U202115514_杨明欣_lab1/bits.c
@@ -0,0 +1,384 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 姓名：杨明欣
+ * 学号：U202115514
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the BDD checker to formally verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+/* 
+ *   lsbZero - set 0 to the least significant bit of x 
+ *   Example: lsbZero(0x87654321) = 0x87654320
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 1
+ */
+int lsbZero(int x) {
+	x = x >> 1;//右移一位
+	x = x << 1;//再左移一位，最后一位为0
+	return x;
+}
+/* 
+ * byteNot - bit-inversion to byte n from word x  
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByteNot(0x12345678,1) = 0x1234A978
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int byteNot(int x, int n) {
+	int t = 0xFF;
+	t = t << (n << 3);//每个字节8位，t滑动窗口
+	return x ^ t;
+}
+/* 
+ *   byteXor - compare the nth byte of x and y, if it is same, return 0, if not, return 1
+
+ *   example: byteXor(0x12345678, 0x87654321, 1) = 1
+
+ *			  byteXor(0x12345678, 0x87344321, 2) = 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 2 
+ */
+int byteXor(int x, int y, int n) {
+	n = n << 3;
+	x = 0xFF & (x >> n);//将对应字节做与、异或
+	y = 0xFF & (y >> n);//将对应字节做与、异或
+	return !!(x ^ y);//若相同则为0,不同则为1，两次取反转换为bool
+}
+/* 
+ *   logicalAnd - x && y
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3 
+ */
+int logicalAnd(int x, int y) {
+  //x&&y,只要有一个为零即为0
+  //正常逻辑为(x | y), 通过两步取非操作，即可转换成逻辑变量
+	return !((!x) | (!y));
+}
+/* 
+ *   logicalOr - x || y
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3 
+ */
+int logicalOr(int x, int y) {
+  //x||y,只要有一个为1即为1
+	return (!(!x)) | (!(!y));
+}
+/* 
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateLeft(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >> !
+ *   Max ops: 25
+ *   Rating: 3 
+ */
+int rotateLeft(int x, int n) {
+	int y = ~((~0) << n); //为取高位准备判别字符
+	x = (x << n) + ((x >> (32 + (~n + 1))) & y);//将高位取出后，加到左移后的结果
+	return x;
+}
+/*
+ * parityCheck - returns 1 if x contains an odd number of 1's
+ *   Examples: parityCheck(5) = 0, parityCheck(7) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int parityCheck(int x) {
+  //将二进制位逐步分成两部分，一一进行异或，结果自然在低位上，低位即为结果
+	x = x ^ (x >> 16); 
+    x = x ^ (x >> 8);
+    x = x ^ (x >> 4);
+    x = x ^ (x >> 2);
+    x = x ^ (x >> 1);
+    return x & 1; //冗余位置0
+}
+/*
+ * mul2OK - Determine if can compute 2*x without overflow
+ *   Examples: mul2OK(0x30000000) = 1
+ *             mul2OK(0x40000000) = 0
+ *         
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 2
+ */
+int mul2OK(int x) {
+    int y = x;
+    y <<= 1; //y = 2x
+    return ((((x ^ y) >> 31) & 0x1) ^ 0x1); //通过符号位是否相同判断溢出情况
+    //return 1;
+}
+/*
+ * mult3div2 - multiplies by 3/2 rounding toward 0,
+ *   Should exactly duplicate effect of C expression (x*3/2),
+ *   including overflow behavior.
+ *   Examples: mult3div2(11) = 16
+ *             mult3div2(-9) = -13
+ *             mult3div2(1073741824) = -536870912(overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int mult3div2(int x) {
+    int t = (x << 1) + x; //t = 3x
+    //首先取出最低位和符号位
+    int l = t & 0x1; 
+    int s = (t >> 31) & 0x1;
+    int ans = (t >> 1); 
+    //通过最低位和符号位判断是否是负数，是否需要补位
+    ans += (l & s); 
+    return ans;
+}
+/* 
+ * subOK - Determine if can compute x-y without overflow
+ *   Example: subOK(0x80000000,0x80000000) = 1,
+ *            subOK(0x80000000,0x70000000) = 0, 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+int subOK(int x, int y) {
+    //减法转换成加法计算，取出结果符号位
+    int z = (x + (~y + 1)) >> 31;
+    //取出两个数的符号位，就可以判断是否溢出
+    x = x >> 31;
+    y = y >> 31;
+    return !((x ^ y) & (x ^ z)) ;
+}
+/* 
+ * absVal - absolute value of x
+ *   Example: absVal(-1) = 1.
+ *   You may assume -TMax <= x <= TMax
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4
+ */
+int absVal(int x) {
+    int y = x>>31;//取出符号位
+    x = (y & (~x + 1)) + ((~y) & x);//符号为正，直接输出，为负，取补输出
+    return x;
+}
+/* 
+ * float_abs - Return bit-level equivalent of absolute value of f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument..
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 10
+ *   Rating: 2
+ */
+unsigned float_abs(unsigned uf) {
+    unsigned x = uf, y = uf;
+    x &= 0x7fffff; //取尾数，形式为原码
+    y >>= 23;
+    y &= 0xff;//取阶码，形式为移码
+    if ((y == 0xff) && (x != 0))
+        return uf;
+    else
+        return uf & 0x7fffffff;
+}
+/* 
+ * float_f2i - Return bit-level equivalent of expression (int) f
+ *   for floating point argument f.
+ *   Argument is passed as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point value.
+ *   Anything out of range (including NaN and infinity) should return
+ *   0x80000000u.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+int float_f2i(unsigned uf) {
+    int x = uf, y = uf, s = uf, z;
+    x &= 0x7fffff;//取尾数，形式为原码
+    y >>= 23;
+    y &= 0xff;//取阶码，形式为移码
+    s >>= 31;//取符号位
+   //判断是否为小数，归0
+    if (y < 127) return 0;
+    //判断阶码为127，实际阶数为0，根据符号位得值
+    if (y == 127) {
+        if (!!s) return -1;
+        else return 1;
+    }
+    y = y - 127;
+    //判断是否溢出，保证移位后有效位数小于31
+    if (y > 30) return 0x80000000;
+    //补全1位
+    x += 0x800000;
+    // 根据解码的情况进行移位
+    if (y < 23)
+        z = x >> (23 - y);
+    else
+        z = x << (y - 23);
+    //按照符号位输出最终结果
+    if (!!s)
+        return ((-1) * z);
+    else
+        return z;
+}
